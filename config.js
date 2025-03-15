8c37d9ef7e9d2b549e51396f3e43bea9ec6f7c22
Tobias <137684165+To-Biobis@users.noreply.github.com>
2025-03-15 13:11:43 +0100

    Aktualisieren von config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ export const consts = {
 	"MAX_PLAYERS": 30,
 	"NEW_PLAYER_LAG": 60,
 	"LEADERBOARD_NUM": 5,
-	"PREFIXES": "Angry Baby Crazy Diligent Excited Fat Greedy Hungry Interesting Japanese Kind Little Magic Naïve Old Powerful Quiet Rich Superman THU Undefined Valuable Wifeless Xiangbuchulai Young Zombie",
-	"NAMES": "Alice Bob Carol Dave Eve Francis Grace Hans Isabella Jason Kate Louis Margaret Nathan Olivia Paul Queen Richard Susan Thomas Uma Vivian Winnie Xander Yasmine Zach"
+	"PREFIXES": "Hurtiger Dummer Süchtiger Reicher Fischkopfartiger Angry Baby Crazy Diligent Excited Fat Greedy Hungry Interesting Japanese Kind Little Magic Naïve Old Powerful Quiet Rich Superman THU Undefined Valuable Wifeless Xiangbuchulai Young Zombie",
+	"NAMES": "Alice Sebastian Pius Tobi Benni Bobber Benny Sebastian Sebastian Sebastian_Bucher  Bob Carol Dave Eve Francis Grace Hans Isabella Jason Kate Louis Margaret Nathan Olivia Paul Queen Richard Susan Thomas Uma Vivian Winnie Xander Yasmine Zach"
 
 };
